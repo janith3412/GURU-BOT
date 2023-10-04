35d849c238137e9026e7b02cd4b58cd51b6de273
janith3412 <143435223+janith3412@users.noreply.github.com>
2023-10-05 01:08:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'Guru_StepsCharm'
 import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
